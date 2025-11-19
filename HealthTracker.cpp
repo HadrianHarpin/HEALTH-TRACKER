f217b7464edf9da18f713f816b241b122d6d1674
AnnisaPartiwi <annisapartiwi.2912@gmail.com>
2025-11-19 22:14:21 +0700

    Update HealthTracker.cpp

--- HealthTracker.cpp
+++ HealthTracker.cpp
@@ -12,11 +12,13 @@ void teksAwal() {
     cout << "                  Konsultasikan Ke Ahli Gizi Untuk Rekomendasi Yang Pasti!                   " << endl;
     cout << "=============================================================================================" << endl;
 }
+
 double hitungBMI(double tinggi, double berat) {
     double tinggiMeter = tinggi / 100.0;
     double BMI = berat / (tinggiMeter * tinggiMeter);
     return BMI; 
 }
+
 void kategoriBMI(double BMI) {
     if (BMI < 18.5) {
         cout << "Kekurangan Berat Badan" << endl;
@@ -27,4 +29,31 @@ void kategoriBMI(double BMI) {
     } else {
         cout << "Obesitas" << endl;
     }
+}
+
+double hitungBMR(double t, double b, double u, char g) {
+    double bmr;
+
+    if (g == 'L' || g == 'l') {
+        bmr = 10 * b + 6.25 * t - 5 * u + 5;
+    } else if(g == 'P' || g == 'p'){
+        bmr = 10 * b + 6.25 * t - 5 * u - 161;
+    }else{
+        cout << "Tidak Dikenali." << endl;
+        bmr = 0.0;
+    }
+    return bmr;
+}
+
+void rekomendasi(double bmi) {
+        cout << "Rekomendasi         : ";
+        if (bmi < 18.5) {
+            cout << "Anda kekurangan berat badan. Tambahkan asupan kalori, perbanyak makanan bergizi";
+        } else if (bmi < 25){
+            cout << "Berat badan anda normal. Pertahankan pola makan sehat dan olahraga teratur.\n";
+        } else if (bmi < 30) {
+            cout << "Anda mengalami kelebihan berat badan. Kurangi asupan kalori (sekitar 300-500 kka)";
+        } else {
+            cout << "Anda obesitas. Disarankan konsultasi ke ahli gizi, kurangi kalori lebih banyak";
+        }
 }
